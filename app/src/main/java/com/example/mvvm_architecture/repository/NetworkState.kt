f62652fda11eab7c929ec48cbd6d9c44c5fa44fb
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-11-07 14:58:13 +0100

    création de la classe NetworkState

--- app/src/main/java/com/example/mvvm_architecture/repository/NetworkState.kt
+++ app/src/main/java/com/example/mvvm_architecture/repository/NetworkState.kt
@@ -1,4 +1,26 @@
 package com.example.mvvm_architecture.data
 
-class NetworkState {
+enum class States {
+    RUNNING,
+    SUCCESS,
+    FAILED
+}
+
+class NetworkState(val state : States, val msg: String) {
+
+    // quand on veut créer quelque chose de static
+    companion object {
+
+        val LOADED : NetworkState
+        val LOADING : NetworkState
+        val ERROR : NetworkState
+
+        // initialisation
+        init {
+            LOADED = NetworkState(States.SUCCESS,  "Success")
+            LOADING = NetworkState(States.RUNNING,  "Running")
+            ERROR = NetworkState(States.FAILED,  "Something went wrong")
+        }
+
+    }
 }
